1061eb58dd7fdab03a441514a530777d1efb5417
Ins <m.saihomi@gmail.com>
2025-03-21 18:14:06 +0800

    Update build.gradle.kts
    jitpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,11 @@
 plugins {
     id("com.android.application") version "8.8.0" apply false
 }
+
+allprojects {
+    repositories {
+        flatDir {
+            dirs 'app/libs'  // ✅ 让 Gradle 识别 `libs/` 目录
+        }
+    }
+}
